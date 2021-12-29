aad874a0fc4046dc801bcfc60cab00b894ee3f06
Postmodern <postmodern.mod3@gmail.com>
2021-12-28 23:46:55 -0800

    Added ronin/FIXME.rb and ronin/FIXME/version.rb files.

---
+++ lib/ronin/FIXME.rb
@@ -0,0 +1,22 @@
+#
+# ronin-FIXME - A library for ronin-rb.
+#
+# Copyright (c) 2021 FIXME (FIXME@FIXME)
+#
+# This file is part of ronin-FIXME.
+#
+# ronin-FIXME is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-FIXME is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-FIXME.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/FIXME/version'
