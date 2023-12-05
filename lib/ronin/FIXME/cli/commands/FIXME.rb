703615a82c7b4aa0b051b1cc7fee27bb125122f2
Postmodern <postmodern.mod3@gmail.com>
2023-12-04 16:35:51 -0800

    Added an example command class.

---
+++ lib/ronin/FIXME/cli/commands/FIXME.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+#
+# ronin-FIXME - A library for ronin-rb.
+#
+# Copyright (c) 2023 FIXME (FIXME@FIXME)
+#
+# ronin-FIXME is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-FIXME is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-FIXME.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/FIXME/cli/command'
+
+module Ronin
+  module FIXME
+    class CLI
+      module Commands
+        #
+        # The `ronin-FIXME FIXME` command.
+        #
+        class FIXME < Command
+
+          usage '[options]'
+
+          description 'FIXME'
+
+          man_page 'ronin-FIXME-FIXME.1'
+
+          #
+          # Runs the `ronin-FIXME fIXME` command.
+          #
+          def run
+          end
+
+        end
+      end
+    end
+  end
+end
